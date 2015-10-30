79e2b2668c85a0cf78fe2c04d4a87548a9e7cc25
Gaurav Tiwari <gaurav@gauravtiwari.co.uk>
2015-10-30 19:55:10 +0000

    Rename model to user

--- db/migrate/20151030195358_devise_create_users.rb
+++ db/migrate/20151030195358_devise_create_users.rb
@@ -1,6 +1,6 @@
 class DeviseCreateModels < ActiveRecord::Migration
   def change
-    create_table(:models) do |t|
+    create_table(:users) do |t|
       ## Database authenticatable
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
@@ -35,9 +35,9 @@ class DeviseCreateModels < ActiveRecord::Migration
       t.timestamps null: false
     end
 
-    add_index :models, :email,                unique: true
-    add_index :models, :reset_password_token, unique: true
-    # add_index :models, :confirmation_token,   unique: true
-    # add_index :models, :unlock_token,         unique: true
+    add_index :users, :email,                unique: true
+    add_index :users, :reset_password_token, unique: true
+    # add_index :users, :confirmation_token,   unique: true
+    # add_index :users, :unlock_token,         unique: true
   end
 end
