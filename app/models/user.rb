fc06871e7adb131105ce5794f915a46dac8931d8
Gaurav Tiwari <gaurav@gauravtiwari.co.uk>
2015-11-04 17:22:45 +0000

    Add associations to models

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,7 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  has_many :posts
+  has_many :comments
 end
