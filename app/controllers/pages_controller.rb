4b2d15d99d22d4f71dd8ac5906256368c981be98
Gaurav Tiwari <gaurav@gauravtiwari.co.uk>
2015-11-15 08:46:44 +0000

    Add pages controller and make it root route

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,7 @@
+class PagesController < ApplicationController
+  protect_from_forgery with: :null_session
+
+  def show
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
 
+  root to: 'pages#show'
+
   scope '/graphql' do
     post "/", to: "graphql#create"
   end
