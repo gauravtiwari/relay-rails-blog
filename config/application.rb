eac33a4e712746e97d3882bdade12e72e4dee0a7
Gaurav Tiwari <gaurav@gauravtiwari.co.uk>
2015-11-29 09:09:58 +0000

    Update application config to use browserify

--- config/application.rb
+++ config/application.rb
@@ -29,5 +29,31 @@ module GraphqlBlog
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.schema_format = :sql
 
+    # Configure react rendering
+    config.react.addons = true
+    config.react.server_renderer_pool_size  ||= 5
+    config.react.server_renderer_timeout    ||= 20
+    config.react.server_renderer = React::ServerRendering::SprocketsRenderer
+    config.react.server_renderer_options = {
+      files: ["react.js", "components.js"],
+      replay_console: true,
+    }
+
+    # Configure rails g to skip helper/assets files
+    config.generators do |g|
+      g.assets = false
+      g.helper = false
+      g.view_specs      false
+      g.helper_specs    false
+    end
+
+    # Setup sidekiq
+    config.active_job.queue_adapter = :sidekiq
+
+    # Setup browserify with Babel
+    config.browserify_rails.commandline_options = "-t babelify --extension=\"es6.js\""
+    config.browserify_rails.source_map_environments << "development"
+    config.browserify_rails.evaluate_node_modules = true
+
   end
 end
