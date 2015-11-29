86d145bd739604ea3c9acc902b035938d1e1e47b
Gaurav Tiwari <gaurav@gauravtiwari.co.uk>
2015-11-29 08:59:14 +0000

    Add seeds and migration

--- db/migrate/20151030195110_devise_create_users.rb
+++ db/migrate/20151030195110_devise_create_users.rb
@@ -4,6 +4,7 @@ class DeviseCreateUsers < ActiveRecord::Migration
       ## Database authenticatable
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
+      t.string :name, null: false
 
       ## Recoverable
       t.string   :reset_password_token
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,40 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+User.create!(
+  name: "Gaurav Tiwari",
+  password: "password",
+  email: "gaurav@gauravtiwari.co.uk"
+)
+
+
+User.create!(
+  name: "Parul Singh",
+  password: "password",
+  email: "parul@parul.co.uk"
+)
+
+
+User.create!(
+  name: "Sheshan Tiwari",
+  password: "password",
+  email: "sheshan@sheshan.co.uk"
+)
+
+10.times do
+  Post.create!(
+    title: "Lorem ipsum",
+    body: "This is body of the post",
+    user_id: (1..3).to_a.sample
+  )
+end
+
+100.times do
+  Comment.create!(
+    body: "This is body of the post",
+    user_id: (1..3).to_a.sample,
+    post_id: (1..10).to_a.sample
+  )
+end
