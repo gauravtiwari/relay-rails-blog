cc9771d3ddfa09ae39b7eec0f4f2d96e2685386f
Gaurav Tiwari <gaurav@gauravtiwari.co.uk>
2015-11-29 09:09:48 +0000

    Update assets

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,3 +1,11 @@
-//= require jquery
-//= require jquery_ujs
+//= require_self
+//= require components
+//= require tracker
 //= require turbolinks
+//= require react_ujs
+
+// Setup React in global scope
+var React = window.React = global.React = require('react/addons');
+window.$ = window.jQuery = require('jquery')
+require('jquery-ujs')
+
