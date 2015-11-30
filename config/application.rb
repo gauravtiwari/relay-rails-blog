30737e5a8a8cb91581376e0e816c94362117c691
Gaurav Tiwari <gaurav@gauravtiwari.co.uk>
2015-11-30 05:17:19 +0000

    Remove duplicate setup for browserify

--- config/application.rb
+++ config/application.rb
@@ -55,8 +55,6 @@ module GraphqlBlog
     config.browserify_rails.source_map_environments << "development"
     config.browserify_rails.evaluate_node_modules = true
 
-
-    config.browserify_rails.evaluate_node_modules = true
-
   end
+
 end
