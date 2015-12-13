e1fa07983df1542415d85b671d761672f2524403
Gaurav Tiwari <gaurav@gauravtiwari.co.uk>
2015-12-13 15:52:05 +0000

    Update readme

--- config/routes.rb
+++ config/routes.rb
@@ -1,24 +1,17 @@
 Rails.application.routes.draw do
 
-  devise_for :users, skip: [:registrations],  controllers: {registrations: 'users/registrations'}
-  as :user do
+  # devise_for :users, skip: [:registrations],  controllers: {registrations: 'users/registrations'}
+  # as :user do
 
-    get   '/join' => 'users/registrations#new',    as: 'new_user_registration'
-    post  '/join' => 'users/registrations#create', as: 'user_registration'
+  #   get   '/join' => 'users/registrations#new',    as: 'new_user_registration'
+  #   post  '/join' => 'users/registrations#create', as: 'user_registration'
 
-  end
-
+  # end
 
   root to: 'pages#home'
-
+  get '/editor', to: 'pages#editor', as: :editor
   scope '/graphql' do
     post "/", to: "graphql#create"
-  end
-
-  get '/editor', to: 'pages#editor', as: :editor
-
-  resources :posts do
-    resources :comments
   end
 
 end
--- readme.md
+++ readme.md
@@ -1,9 +1,13 @@
-### WIP: Webblog powered by Rails, react, graphql and Relay.
+### WIP: Blog application powered by Rails, react, graphql and Relay.
 
+This blog is an exploration excerise to understand two new technologies - GraphQL and Relay. It's a work in progress and this repository will update as we move further with this application.
+
+### Running locally
 To run the application, please just clone the repo and run it like so:
 
 ```
 git clone git@github.com:gauravtiwari/relay-rails-blog.git
 cd relay-rails-blog
+npm install
 ./start (from terminal). If you get permission error, just do chmod 777 start
 ```
