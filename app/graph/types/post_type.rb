7322e4861ea6470e2a4352dcf4bd6b967e3adac5
Gaurav Tiwari <gaurav@gauravtiwari.co.uk>
2015-12-25 19:57:19 +0000

    Update style and HTML format for post body

--- app/graph/types/post_type.rb
+++ app/graph/types/post_type.rb
@@ -10,7 +10,6 @@ PostType = GraphQL::ObjectType.define do
   # Expose fields associated with Post model
   field :title, types.String, "The title of this post"
   field :slug, types.String, "The slug of this post"
-  field :body, types.String,  "The body of this post"
   field :comments_count, types.String,  "The total numner of comments on this post"
   field :votes_count, types.String,  "The total numner of votes on this post"
   field :created_at, types.String, "The time at which this post was created"
@@ -30,6 +29,14 @@ PostType = GraphQL::ObjectType.define do
     description "The short description of this post"
     resolve -> (object, arguments, context) {
       truncate(object.body, length: 150, escape: false)
+    }
+  end
+
+  field :body do
+    type types.String
+    description "The body of this post"
+    resolve -> (object, arguments, context) {
+      simple_format(object.body)
     }
   end
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -11,9 +11,9 @@ end
 100.times do
   Post.create!(
     title: Faker::Lorem.sentence,
-    body: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eaque, incidunt! Voluptatibus quasi asperiores veritatis nesciunt vitae aliquid, praesentium ratione. Repudiandae, dolor, incidunt. Amet corporis porro eveniet rem, eligendi vero, quae.
-        Science cuts two ways, of course; its products can be used for both good and evil. But there's no turning back from science. The early warnings about technological dangers also come from science.
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam quo autem nisi nobis nemo labore explicabo repellat deserunt harum sequi amet odit, alias reiciendis quia, incidunt vel ullam totam rem?
+    body: "<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eaque, incidunt! Voluptatibus quasi asperiores veritatis nesciunt vitae aliquid, praesentium ratione. Repudiandae, dolor, incidunt. Amet corporis porro eveniet rem, eligendi vero, quae.</p>
+        <p>Science cuts two ways, of course; its products can be used for both good and evil. But there's no turning back from science. The early warnings about technological dangers also come from science.</p>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam quo autem nisi nobis nemo labore explicabo repellat deserunt harum sequi amet odit, alias reiciendis quia, incidunt vel ullam totam rem?</p>
       ",
     user_id: (1..10).to_a.sample
   )
