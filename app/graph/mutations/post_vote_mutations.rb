77ac1ab2e4b27cfa27fd50fe24c84a1a0b6df710
Gaurav Tiwari <gaurav@gauravtiwari.co.uk>
2016-01-01 15:26:26 +0000

    Add mutations for vote

---
+++ app/graph/mutations/post_vote_mutations.rb
@@ -0,0 +1,39 @@
+module PostVoteMutations
+  Create = GraphQL::Relay::Mutation.define do
+    name "CreatePostVote"
+
+    input_field :votable_id, !types.ID
+
+    return_field :post, PostType
+
+    resolve -> (inputs, ctx) {
+      votable = NodeIdentification.object_from_id_proc.call(inputs[:votable_id], ctx)
+      user = ctx[:current_user]
+      votable.votes.create({
+        user: user
+      })
+
+      { post: votable }
+    }
+  end
+
+  Destroy = GraphQL::Relay::Mutation.define do
+    name "DestroyPostVote"
+
+    input_field :votable_id, !types.ID
+
+    return_field :post, PostType
+
+    resolve -> (inputs, ctx) {
+      votable = NodeIdentification.object_from_id_proc.call(inputs[:votable_id], ctx)
+      user = ctx[:current_user]
+      vote = user.votes.where({
+        votable: votable
+      }).first
+      vote.destroy
+
+      { post: votable }
+    }
+  end
+end
+
