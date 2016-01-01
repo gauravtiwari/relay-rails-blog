8506c2431a655cdcb22a51b00f2cb8294a091947
Gaurav Tiwari <gaurav@gauravtiwari.co.uk>
2016-01-01 18:24:03 +0000

    Cleanup routes for part 1

--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,9 @@
 Rails.application.routes.draw do
-
+  root to: 'pages#home'
   devise_for :users, skip: [:registrations],  controllers: {registrations: 'users/registrations'}
   as :user do
-
     get   '/signup' => 'users/registrations#new',    as: 'new_user_registration'
     post  '/signup' => 'users/registrations#create', as: 'user_registration'
-
   end
-
-  root to: 'pages#home'
-  get '/editor', to: 'pages#editor', as: :editor
-  scope '/graphql' do
-    post "/", to: "graphql#create"
-  end
-
   resources :posts
-
 end
