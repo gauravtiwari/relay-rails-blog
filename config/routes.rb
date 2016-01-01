a476ec94e46ac97eb51a0e2b64ddeb96b4ed9e5a
Gaurav Tiwari <gaurav@gauravtiwari.co.uk>
2016-01-01 13:00:55 +0000

    Update routes for signup

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,8 @@ Rails.application.routes.draw do
   devise_for :users, skip: [:registrations],  controllers: {registrations: 'users/registrations'}
   as :user do
 
-    get   '/join' => 'users/registrations#new',    as: 'new_user_registration'
-    post  '/join' => 'users/registrations#create', as: 'user_registration'
+    get   '/signup' => 'users/registrations#new',    as: 'new_user_registration'
+    post  '/signup' => 'users/registrations#create', as: 'user_registration'
 
   end
 
