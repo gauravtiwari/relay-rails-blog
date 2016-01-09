c4bd382f362e1d4acdb189d16752d16703e59acd
Gaurav Tiwari <gaurav@gauravtiwari.co.uk>
2016-01-09 11:48:35 +0000

    Update graphql controller to fetch current user

--- app/controllers/graphql_controller.rb
+++ app/controllers/graphql_controller.rb
@@ -1,11 +1,21 @@
 class GraphqlController < ApplicationController
   # Ignore CSRF, rely on some auth token
   protect_from_forgery :except => [:create]
+  before_action :set_current_user
 
   def create
-    verified_user = User.find_by(id: cookies.signed["user.id"])
-    result = RelaySchema.execute(params[:query], debug: true, variables: params[:variables], context: {current_user: verified_user})
+    result = RelaySchema.execute(params[:query], debug: true, variables: params[:variables], context: {current_user: set_current_user})
     render json: result
+  end
+
+  private
+
+  def set_current_user
+    if verified_user = User.find_by(id: cookies.signed['user.id'])
+      verified_user
+    else
+      nil
+    end
   end
 
 end
