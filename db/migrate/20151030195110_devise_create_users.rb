b43c15312d501fddb8c265685d7bce91ae8dbb4b
Gaurav Tiwari <gaurav@gauravtiwari.co.uk>
2016-01-18 17:07:40 +0000

    Remove confirmation

--- db/migrate/20151030195110_devise_create_users.rb
+++ db/migrate/20151030195110_devise_create_users.rb
@@ -42,7 +42,5 @@ class DeviseCreateUsers < ActiveRecord::Migration
 
     add_index :users, :email,                unique: true
     add_index :users, :reset_password_token, unique: true
-    # add_index :users, :confirmation_token,   unique: true
-    # add_index :users, :unlock_token,         unique: true
   end
 end
