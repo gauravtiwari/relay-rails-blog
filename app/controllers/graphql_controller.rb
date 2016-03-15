7b2467f81df07ecccce61197b2f7a2ea4805e639
Gaurav Tiwari <gaurav@gauravtiwari.co.uk>
2016-03-15 13:33:42 +0100

    Skip csrf for now

--- app/controllers/graphql_controller.rb
+++ app/controllers/graphql_controller.rb
@@ -1,4 +1,5 @@
 class GraphqlController < ApplicationController
+  skip_before_action :verify_authenticity_token
   before_action :set_current_user
 
   def create
@@ -22,5 +23,4 @@ class GraphqlController < ApplicationController
       nil
     end
   end
-
 end
--- config/initializers/graphiql.rb
+++ config/initializers/graphiql.rb
@@ -1,1 +1,3 @@
 GraphiQL::Rails.config.query_params = true
+GraphiQL::Rails.config.csrf = true
+GraphiQL::Rails.config.initial_query = GraphiQL::Rails::WELCOME_MESSAGE
