0ef050457f3283804fe576e3e9a1a6a04e433599
Gaurav Tiwari <gaurav@gauravtiwari.co.uk>
2016-07-18 16:27:30 +0100

    Move type generator to a model concern

---
+++ app/models/concerns/graphqlable.rb
@@ -0,0 +1,45 @@
+module Graphqlable
+  extend ActiveSupport::Concern
+
+  module ClassMethods
+    def to_graphql_type(fields=[])
+      model = self
+      model_fields = model.columns_hash
+      GraphQL::ObjectType.define do
+        name(model.name)
+        description("A single #{model.name} type")
+        interfaces [NodeIdentification.interface]
+        global_id_field(:id)
+
+        # Check if the fields are given or use default fields
+        @exposed_fields = if fields.any?
+          model_fields.select do |column|
+            fields.include?(column.to_sym)
+          end
+        else
+          model_fields
+        end
+
+        # Make the fields available for this model
+        @exposed_fields.each do |column, type|
+          field(
+            column,
+            convert_type(type),
+            "#{column} field for the #{model.name} model"
+          )
+        end
+      end
+    end
+  end
+end
+
+def convert_type(database_type)
+  case database_type
+  when :integer
+    types.Int
+  when :boolean
+    types.Boolean
+  else
+    types.String
+  end
+end
