4180b3863697a57f95032fcbd1753548de1572c6
Gaurav Tiwari <gaurav@gauravtiwari.co.uk>
2016-07-18 16:27:47 +0100

    Update posts resolver

--- app/api/resolvers/posts_resolver.rb
+++ app/api/resolvers/posts_resolver.rb
@@ -1,11 +1,11 @@
 class PostsResolver
   def call(_, args, _)
     if args[:tag]
-      ::Post.where("'#{args[:tag]}' = ANY (tags)").includes(:user).order(args[:order])
+      Post.where("'#{args[:tag]}' = ANY (tags)").includes(:user).order(args[:order])
     elsif args[:filter]
-      ::Post.send(args[:filter]).includes(:user).order(args[:order])
+      Post.send(args[:filter]).includes(:user).order(args[:order])
     else
-      ::Post.includes(:user).order(args[:order])
+      Post.includes(:user).order(args[:order])
     end
   end
 end
